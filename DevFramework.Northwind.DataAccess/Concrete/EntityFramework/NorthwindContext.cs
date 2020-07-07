aff70117fb2dcfbfe04d105fc06758021211b8b0
furkanaltaca <furkanaltaca@gmail.com>
2020-07-07 23:47:50 +0300

    EntityFramework configurations has been added.

---
+++ DevFramework.Northwind.DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,19 @@
+﻿using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.DataAccess.Concrete.EntityFramework
+{
+    public class NorthwindContext : DbContext
+    {
+        public NorthwindContext()
+        {
+            Database.SetInitializer<NorthwindContext>(null); //veritabanında otomatik olarak veri oluşturmayı engeller
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
